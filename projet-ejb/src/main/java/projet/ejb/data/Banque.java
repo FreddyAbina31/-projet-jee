4c78570ac82d0a75f58a31be5987e543f780d453
Toubson20 <toubiouf7@gmail.com>
2023-12-29 14:07:32 +0100

    Merge branch 'master' of https://github.com/FreddyAbina31/-projet-jee.git

---
+++ projet-ejb/src/main/java/projet/ejb/data/Banque.java
@@ -0,0 +1,76 @@
+package projet.ejb.data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * The persistent class for the banque database table.
+ * 
+ */
+@Entity
+@Table(name = "banque")
+public class Banque {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String nom;
+
+	private BigDecimal solde;
+
+	// bi-directional many-to-one association to Utilisateur
+	@OneToMany(mappedBy = "banque")
+	private List<Utilisateur> utilisateurs;
+
+	public Banque() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public BigDecimal getSolde() {
+		return this.solde;
+	}
+
+	public void setSolde(BigDecimal solde) {
+		this.solde = solde;
+	}
+
+	public List<Utilisateur> getUtilisateurs() {
+		return this.utilisateurs;
+	}
+
+	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
+		this.utilisateurs = utilisateurs;
+	}
+
+	public Utilisateur addUtilisateur(Utilisateur utilisateur) {
+		getUtilisateurs().add(utilisateur);
+		utilisateur.setBanque(this);
+
+		return utilisateur;
+	}
+
+	public Utilisateur removeUtilisateur(Utilisateur utilisateur) {
+		getUtilisateurs().remove(utilisateur);
+		utilisateur.setBanque(null);
+
+		return utilisateur;
+	}
+
+}
---
+++ projet-ejb/src/main/java/projet/ejb/data/Mouvement.java
@@ -0,0 +1,77 @@
+package projet.ejb.data;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the mouvement database table.
+ * 
+ */
+@Entity
+@Table(name = "mouvement")
+public class Mouvement {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="date_transaction")
+	private Date dateTransaction;
+
+	@Column(name="heure_transaction")
+	private Time heureTransaction;
+
+	private String libelle;
+
+	//bi-directional many-to-one association to Utilisateur
+	@ManyToOne
+	@JoinColumn(name="id_utilisateur")
+	private Utilisateur utilisateur;
+
+	public Mouvement() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDateTransaction() {
+		return this.dateTransaction;
+	}
+
+	public void setDateTransaction(Date dateTransaction) {
+		this.dateTransaction = dateTransaction;
+	}
+
+	public Time getHeureTransaction() {
+		return this.heureTransaction;
+	}
+
+	public void setHeureTransaction(Time heureTransaction) {
+		this.heureTransaction = heureTransaction;
+	}
+
+	public String getLibelle() {
+		return this.libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return this.utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+
+}
