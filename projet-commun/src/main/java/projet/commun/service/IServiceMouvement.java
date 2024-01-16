83c7995642a960b885b68bc76ff4402e0a9f42c9
Toubson20 <toubiouf7@gmail.com>
2024-01-16 17:07:03 +0100

    update

--- projet-commun/src/main/java/projet/commun/service/IServiceMouvement.java
+++ projet-commun/src/main/java/projet/commun/service/IServiceMouvement.java
@@ -6,7 +6,6 @@ import projet.commun.dto.DtoMouvement;
 import projet.commun.exception.ExceptionValidation;
 
 public interface IServiceMouvement {
-<<<<<<< HEAD
 
 	int inserer(DtoMouvement dtoMouvement) throws ExceptionValidation;
 
@@ -17,16 +16,4 @@ public interface IServiceMouvement {
 	DtoMouvement retrouver(int idMouvement);
 
 	List<DtoMouvement> listerTout();
-
-=======
-	int				inserer( DtoMouvement dtoMouvement ) throws ExceptionValidation;
-
-	void			modifier( DtoMouvement dtoMouvement ) throws ExceptionValidation; 
-
-	void			supprimer( int idProduit ) throws ExceptionValidation;
-
-	DtoMouvement 		retrouver( int idProduit ) ;
-
-	List<DtoMouvement>	listerTout() ;
->>>>>>> b51633555e5204a6e21b550745e3d28f3410430f
 }
